2fd5ac8aa8babebaef5f30013deb02cc3c5ea4a5
Greg4016 <gerike.kiss85@gmail.com>
2024-10-15 23:02:37 +0200

    new functions

--- content.js
+++ content.js
@@ -8,6 +8,24 @@ window.addEventListener('load', () => {
         qsa: function (selector) {
             return document.querySelectorAll(selector)
         },
+        findText: function (elem, text) {
+            let result = []
+    
+            let ind = elem.outerHTML.search(text)
+            if(ind != -1) {
+                result.push([elem, ind, elem.innerText.search(text)]);
+        
+                [...elem.children].forEach((ch) => {   
+                    let res = findText(ch, text)
+                    if(res.length > 0) result.push(...res)
+                })
+            }
+        
+            return result
+        },
+        log: function (data) {
+            console.log(data)
+        }
     };
   
     // Add global functions
